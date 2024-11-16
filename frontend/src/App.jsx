fa1b9c11eaaa5312c88dc635afdb9605a956fc24
Zigmas Petrauskas <zigmas.1@gmail.com>
2024-11-16 20:28:30 +0200

    Sukurtas header ir pritaikytas mobile first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-
+// import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import "./index.scss";
 
 const App = () => {
-  
-
   return (
-      <div>
-        Labas
-      </div>
+    <>
+      <Header />
+    </>
   );
 };
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../assets/Logo_1.webp";
+import "./Header.scss";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="Zigmaswebdev logo" />
+        <div className="logo-text">Zigmas WebDev</div>
+      </div>
+      <nav className={isMenuOpen ? "open" : ""} aria-label="Main Navigation">
+        <ul>
+          <li>
+            <Link to="/" onClick={closeMenu}>Pagrindinis</Link>
+          </li>
+          <li>
+            <Link to="/apie-mane" onClick={closeMenu}>Apie mane</Link>
+          </li>
+          <li>
+            <Link to="/atsiliepimai" onClick={closeMenu}>Atsiliepimai</Link>
+          </li>
+          <li>
+            <Link to="/kontaktai" onClick={closeMenu}>Kontaktai</Link>
+          </li>
+        </ul>
+      </nav>
+      <button className="menu-toggle" onClick={toggleMenu} aria-label="Toggle Menu">
+        {isMenuOpen ? <FaTimes /> : <FaBars />}
+      </button>
+    </header>
+  );
+};
+
+export default Header;
